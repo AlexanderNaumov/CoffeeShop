f400026f523983f7b23e1700264728ad6629ee70
Alexander <haymob@me.com>
2022-11-11 18:04:53 +0300

    android.fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -34,21 +34,23 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = "1.3.2"
     }
 }
+
+val composeVersion = "1.3.0"
 
 dependencies {
     implementation(project(":core"))
     implementation("io.insert-koin:koin-core:3.2.0")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.compose.ui:ui:1.3.0")
-    implementation("androidx.compose.material:material:1.3.0")
-    implementation("androidx.compose.material:material-icons-extended:1.3.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
 //    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.activity:activity-compose:1.6.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
 //    implementation("androidx.compose.foundation:foundation:1.2.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("io.coil-kt:coil-compose:2.2.0")
